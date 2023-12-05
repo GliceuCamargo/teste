7968c8219b34bdecdb2e71d839bf4a2e8c7d9c15
GliceuCamargo <67240186+GliceuCamargo@users.noreply.github.com>
2023-12-05 13:07:43 -0300

    Initial commit

---
+++ teste.js
@@ -0,0 +1,38 @@
+const obj={
+    nome:"Gliceu",
+    idade: 58,
+    email: "gliceu@hotmail.com"
+}
+
+for (const key in obj) {
+    console.log(key);
+    console.log(obj[key]);
+}
+const arr1= ["gliceu", "lucinae", "Bruna", "Rafa", "kaua"]
+for (const iterator of arr) {
+    console.log(arr)
+}
+
+// const arr= new Array(1, "gliceu", "luciane", 'kaua', new Array(345, "estudo", false), "testando");
+// arr.pop();
+// arr.pop();
+// arr.push("acertando a parada", "", 45)
+// console.log(arr);
+
+
+// const obj=new Object()
+// obj.nome="gliceu F";
+// obj.idade=58;
+// console.log(obj["nome"] + "    "+ obj.idade)
+// console.log(obj);
+// const pessoa={
+//     nome:"Luciane",
+//     idade: 51
+// }
+// console.log(pessoa);
+
+// const array= new Array("Bruna", "gliceu", "luciane", 'kaua', "Rafael", "Luzia");
+// console.log(array);
+// for (let index = 0; index < array.length; index++) {
+//     console.log(`${index+1}   meu nome é: ${array[index]}`);    
+// }
